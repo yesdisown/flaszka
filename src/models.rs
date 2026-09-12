55d2732091b64141c285999c07801c4ed8b0af46
yd <yesdisown@proton.me>
2026-09-13 01:48:42 +0200

    initialize structs

--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,22 @@
+use std::collections::HashMap;
 
+pub struct Card {
+    pub front: String,
+    pub back: String,
+    pub fields: HashMap<String, String>,
+}
+
+pub struct Deck {
+    pub name: String,
+    pub cards: Vec<Card>,
+}
+
+pub struct Shelf {
+    pub id: u8,
+    pub cards: Vec<Card>,
+}
+
+pub struct Chest {
+    pub name: String,
+    pub shelves: Vec<Shelf>,
+}
