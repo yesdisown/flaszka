67b3eaae9a9a7673aa6238c217d09b4100379e93
yd <yesdisown@proton.me>
2026-09-13 23:18:45 +0200

    add a find id by card function, and a bit of a refactor

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(PartialEq, Eq)]
 pub struct Card {
     pub front: String,
     pub back: String,
@@ -19,4 +20,16 @@ pub struct Shelf {
 pub struct Chest {
     pub name: String,
     pub shelves: Vec<Shelf>,
+}
+
+impl Shelf {
+    pub fn find_id_by_card(&self, card: Card) -> Option<usize> {
+        self.cards.iter().position(|c| c == &card)
+    }
+}
+
+impl Chest {
+    pub fn get_flashcard(&mut self) -> Option<Card> {
+        self.shelves.first_mut()?.cards.pop()
+    }
 }
