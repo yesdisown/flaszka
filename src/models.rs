2927639db989053e372e7c59b1e9592fffda2668
yd <yesdisown@proton.me>
2026-09-15 01:24:56 +0200

    add a few methods for manipulating Chests and Shelves, but they'll need to be changed a bit soon

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-#[derive(PartialEq, Eq)]
+pub enum MoveTarget {
+    First,
+    Next,
+}
 pub struct Card {
     pub front: String,
     pub back: String,
@@ -13,7 +16,6 @@ pub struct Deck {
 }
 
 pub struct Shelf {
-    pub id: u8,
     pub cards: Vec<Card>,
 }
 
@@ -23,13 +25,43 @@ pub struct Chest {
 }
 
 impl Shelf {
-    pub fn find_id_by_card(&self, card: Card) -> Option<usize> {
-        self.cards.iter().position(|c| c == &card)
+    /// returns a refference to the first card of the shelf
+    pub fn top_card(&self) -> Option<&Card> {
+        self.cards.first()
     }
 }
 
 impl Chest {
-    pub fn get_flashcard(&mut self) -> Option<Card> {
-        self.shelves.first_mut()?.cards.pop()
+    /// returns the id of the first non-empty shelf
+    fn top_shelf_id(&self) -> Option<usize> {
+        Some(
+            self.shelves
+                .iter()
+                .enumerate()
+                .find(|(_, s)| !s.cards.is_empty())?
+                .0,
+        )
+    }
+
+    /// moves the top card of the first non-empty shelf of the chest.
+    /// returns None if chest is empty
+    pub fn move_top_card(&mut self, target: MoveTarget) -> Option<()> {
+        // because of the ? operator, if the chest is empty None will be returned here
+        let top_shelf_id = self.top_shelf_id()?;
+        let top_card = self.shelves.get_mut(top_shelf_id).unwrap().cards.remove(0);
+
+        let target_shelf_id = match target {
+            MoveTarget::First => 0,
+            MoveTarget::Next if self.shelves.len() == top_shelf_id => top_shelf_id,
+            MoveTarget::Next => top_shelf_id + 1,
+        };
+
+        self.shelves
+            .get_mut(target_shelf_id)
+            .unwrap()
+            .cards
+            .push(top_card);
+
+        Some(())
     }
 }
